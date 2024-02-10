91aaed3df9521fa3ce4254d2cb378f80be6b1b3c
Adwitheya <76691277+Adwitheya@users.noreply.github.com>
2024-02-10 16:00:31 -0500

    Adding skeleton code for ColorHistogram class

---
+++ Part 1/ColorHistogram.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class ColorHistogram {
+  /**
+  * @brief Parameterized Constructor to make a histogram for a d-bit image
+  * @param d color depth of image in bits; 3 bits = 512 colors
+  */
+  public ColorHistogram (int d) {
+
+  }
+
+  /**
+  * @brief Parameterized Constructor to make a histogram from a text file
+  * @param filename name of the text file where histogram data is stored
+  */
+  public ColorHistogram (String filename) {
+
+  }
+
+  /**
+  * @brief Method to associate image with histogram instance
+  * @param image ColorImage instance to be liked to a histogram object
+  */
+  public void setImage (ColorImage image) {
+
+  }
+
+  /**
+  * @brief Returns normalized histogram of the image
+  */
+  public double[] getHistogram () {
+
+  }
+
+  /**
+  * @brief Returns intersection between two histograms
+  * @param hist the histogram to compare the present histogram instance with
+  */
+  public double compare (ColorHistogram hist) {
+
+  }
+
+  /**
+  * @brief Saves the current histogram information in a text file
+  * @param filename name of the file to store histogram data
+  */
+  public void save (String filename) {
+
+  }
+
+}
